2991b5606fc7a3725985cebc183336d10164d34b
Paco Estrada <pacoestrada77@gmail.com>
2023-03-27 00:03:07 +0200

    tercera version del ejercicio 2

--- Entrega del ejercicio 2.py
+++ Entrega del ejercicio 2.py
@@ -1,17 +1,16 @@
-mes = input("Introduce el nombre de un mes: ")
-mes = mes.lower()
+mes_usuario = input("Introduce el nombre de un mes: ")
+mes = mes_usuario.lower()
 
 if  (mes == "enero" or mes == "marzo" or mes == "mayo" or mes == "julio" or mes == " agosto" or mes == "Octubre" or mes == "diciembre"):
 
-    print("El mes de", mes, "tiene 31 días.")
+    print("El mes de", mes_usuario, "tiene 31 días.")
 
 elif mes == "febrero":
-    print("El mes de", mes, "tiene 28 días.")
+    print("El mes de", mes_usuario, "tiene 28 días.")
 
 elif (mes == "abril" or mes == "junio" or mes == "septiembre" or mes == "noviembre"):
-    dias = 30
 
-    print("El mesde", mes, "tiene 30 días.")
+    print("El mes de", mes_usuario, "tiene 30 días.")
 
 else:
     print("No conozco ese mes.")
