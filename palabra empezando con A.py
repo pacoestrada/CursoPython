400a6be3949f1d018bd480941c5291d44f24ad2a
Paco Estrada <pacoestrada77@gmail.com>
2023-03-28 12:00:30 +0200

    Version 2.0 comentada

--- palabra empezando con A.py
+++ palabra empezando con A.py
@@ -1,9 +1,11 @@
 #pedimos al usuario que escriba una palabra
 
 palabra = input("Introduce una palabra: ")
+#iniciamos un contador
 contador = 1
+#comparamos hasta que se cumpla, aumentando el contador a uno
 while "a" != palabra[0]:
     contador = contador + 1
-    palabra = (input("tu palabra no empieza por A o a .Escribe otra:"))
-
+    palabra = (input("Tu palabra no empieza por A o a .Escribe otra:"))
+#imprimimos resultado
 print("Tu palabra empieza por A o a. Para ello has usado" , contador , "palabras" )
