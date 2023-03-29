37937d95c36515bacb3111ed6d35e575232668ce
Paco Estrada <pacoestrada77@gmail.com>
2023-03-29 11:58:41 +0200

    Version 3 de entrega

--- Entrega del ejercicio3.py
+++ Entrega del ejercicio3.py
@@ -1,38 +1,40 @@
 # Crear lista de gastos vacía
 gastos = []
 
-# Preguntar al usuario si quiere introducir más gastos
+# Preguntar al usuario si quiere indicar más gastos
 continuar = input("Quieres introducir más gastos (si/no): ")
 
-# Mientras haya más gastos
+# Siempre que haya más gastos (respuesta si)
 while continuar.lower() == "si":
-    # Pedir datos del gasto al usuario
+
+# Pedimos los datos del gasto al usuario
     motivo = input("Motivo del gasto: ")
     lugar = input("Lugar del gasto: ")
     cantidad = float(input("Cantidad gastada: "))
 
-    # Crear diccionario con la información del gasto
+#Creamos un diccionario que contenga la información del gasto
     gasto = {"motivo": motivo, "lugar": lugar, "cantidad": cantidad}
 
-    # Añadir el diccionario a la lista de gastos
+#Añadimos diccionario a la lista de gastos
     gastos.append(gasto)
 
-    # Preguntar al usuario si quiere introducir más gastos
+#Preguntamos al usuario si quiere introducir más gastos
     continuar = input("Quieres introducir más gastos (si/no): ")
 
 cantidad_total = 0
+
 # Para cada gasto en la lista de gastos
 print("\n")
 print("GASTOS DESGLOSADOS Y TOTAL.\n")
 for gasto in gastos:
-    # Imprimir información del gasto
+#Salida con la información del gasto
     print(gasto['motivo'] + ' en ' + gasto['lugar'] + ' : ' + str(gasto['cantidad']) + ' euros')
 
-    # Acumular la cantidad del gasto en cantidad_total
+# Acumulamos la cantidad del gasto en cantidad_total
     cantidad_total += gasto["cantidad"]
 
-# Imprimir cantidad_total
-print("Cantidad total : " + str(cantidad_total) + "euros")
+# Imprimir CANIDAD TOTAL acumulada en "cantidad_total", convertida en string
+print("CANTIDAD TOTAL : " + str(cantidad_total) + "euros")
 
 
 
