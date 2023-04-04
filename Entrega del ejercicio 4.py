724f02b9253388f9e3e68b0b67550b72848d221d
Paco Estrada <pacoestrada77@gmail.com>
2023-04-04 12:56:01 +0200

    Version de entrega del ejercicio 4

--- Entrega del ejercicio 4.py
+++ Entrega del ejercicio 4.py
@@ -1,22 +1,25 @@
+
+#definimos la función tipo_tiángulo
 def tipo_triangulo():
     """
-     Función que olicita al usuario que introduzca las longitudes de 3 lados y determina si es posible formar un triángulo con los valores introducidos.
-     Si se puede, nos devuelve si es un triángulo equilátero, isósceles o escaleno, o si no es un triángulo.
+     Función que solicita al usuario que introduzca las longitudes de 3 lados y determina si es posible formar un triángulo con los valores introducidos.
+     Si se puede, nos devuelve si es un triángulo equilátero, isósceles o escaleno o, ensu caso, si no es un triángulo.
     """
-    a = float(input("Ingrese la longitud del primer lado: "))
-    b = float(input("Ingrese la longitud del segundo lado: "))
-    c = float(input("Ingrese la longitud del tercer lado: "))
+    a = float(input("Ingrese la longitud del primer lado y pulse ENTER: "))
+    b = float(input("Ingrese la longitud del segundo lado y pulse ENTER: "))
+    c = float(input("Ingrese la longitud del tercer lado y pulse ENTER: "))
 
     if a + b > c and a + c > b and b + c > a:
         if a == b and b == c:
-            print("El triangulo definido por esos tres lados es Equilátero") #caso equilatero
+            print("\n El triangulo definido por esos tres lados es EQUILATERO") #caso equilatero
         elif a == b or b == c or a == c:
-            print("El triangulo definido por esos tres lados es Isósceles") #caso isóceles
+            print("\n El triangulo definido por esos tres lados es ISÓSCELES") #caso isóceles
         else:
-            print("El triangulo definido por esos tres lados es Escaleno")  #caso escaleno
+            print("\n El triangulo definido por esos tres lados es ESCALENO")  #caso escaleno
     else:
-        print("El triangulo definido por esos tres lados es No es un triángulo") # No es triángulo
+        print("\n El triangulo definido por esos tres lados es NO ES UN TIÁNGULO") # No es triángulo
 
+#llamamos a la función tipo_tirangulo()
 
 tipo_triangulo()
 
