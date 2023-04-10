099530f9ea836366d01e1c411b4d474ae9943226
Paco Estrada <pacoestrada77@gmail.com>
2023-04-10 09:46:30 +0200

    Version  2 de entrega del ejercicio 5

---
+++ Entrega del ejercicio 5.py
@@ -0,0 +1,37 @@
+import csv
+
+def calcula_notas_finales(nombre_archivo):
+    """
+    Calcula las notas finales de un conjunto de alumnos a partir de un archivo CSV con el formato especificado.
+
+    :param nombre_archivo: el nombre del archivo CSV a procesar.
+    :return: None
+    """
+    with open(nombre_archivo, newline='', encoding='utf-8') as archivo:
+        lector_csv = csv.reader(archivo)
+        # Saltar la primera línea (encabezados)
+        next(lector_csv)
+        for linea in lector_csv:
+            nombre = linea[0]
+            apellidos = linea[1]
+            control1 = float(linea[2])
+            control2 = float(linea[3])
+            examen_final = float(linea[4])
+
+            # Calcular la nota final
+            nota_final = 0
+            if examen_final >= 5:
+                nota_final = 0.1 * control1 + 0.1 * control2 + 0.8 * examen_final
+            else:
+                nota_final = examen_final
+
+            # Determinar si el alumno aprobó o no
+            aprobado = False
+            if nota_final >= 5:
+                aprobado = True
+
+            # Imprimir la información del alumno
+            print(nombre, apellidos, round(nota_final, 2), 'APROBADO' if aprobado else 'SUSPENSO')
+
+# Ejemplo de uso
+calcula_notas_finales('notas.csv')
---
+++ Triangulos con Tkinter.py
@@ -0,0 +1,61 @@
+import tkinter as tk
+
+
+def tipo_triangulo():
+    try:
+        a = float(entry_a.get())
+        b = float(entry_b.get())
+        c = float(entry_c.get())
+
+        if a + b > c and a + c > b and b + c > a:
+            if a == b == c:
+                resultado.config(text="Es un triángulo equilátero", fg="green")
+            elif a == b or a == c or b == c:
+                resultado.config(text="Es un triángulo isósceles", fg="green")
+            else:
+                resultado.config(text="Es un triángulo escaleno", fg="green")
+        else:
+            resultado.config(text="No se puede formar un triángulo", fg="red")
+    except ValueError:
+        resultado.config(text="Los valores introducidos no son numéricos", fg="red")
+
+
+# Crear ventana
+ventana = tk.Tk()
+ventana.title("Calculadora de Triángulos")
+ventana.config(bg="lightblue")
+
+# Crear frames
+frame_info = tk.Frame(ventana, bg="lightblue")
+frame_info.pack(pady=20)
+frame_inputs = tk.Frame(ventana, bg="lightblue")
+frame_inputs.pack(pady=20)
+frame_resultado = tk.Frame(ventana, bg="lightblue")
+frame_resultado.pack(pady=20)
+
+# Crear widgets en frame_info
+tk.Label(frame_info,
+         text="Este programa calcula el tipo de triángulo que se forma\na partir de las medidas de sus lados.",
+         bg="lightblue", font=("Arial", 20)).pack()
+
+# Crear widgets en frame_inputs
+tk.Label(frame_inputs, text="Lado a: ", bg="lightblue", font=("Arial", 16)).grid(row=0, column=0, padx=10, pady=5)
+entry_a = tk.Entry(frame_inputs, font=("Arial", 16), width=10)
+entry_a.grid(row=0, column=1, padx=10, pady=5)
+
+tk.Label(frame_inputs, text="Lado b: ", bg="lightblue", font=("Arial", 16)).grid(row=1, column=0, padx=10, pady=5)
+entry_b = tk.Entry(frame_inputs, font=("Arial", 16), width=10)
+entry_b.grid(row=1, column=1, padx=10, pady=5)
+
+tk.Label(frame_inputs, text="Lado c: ", bg="lightblue", font=("Arial", 16)).grid(row=2, column=0, padx=10, pady=5)
+entry_c = tk.Entry(frame_inputs, font=("Arial", 16), width=10)
+entry_c.grid(row=2, column=1, padx=10, pady=5)
+
+# Crear widget en frame_resultado
+resultado = tk.Label(frame_resultado, bg="lightblue", font=("Arial", 20))
+resultado.pack()
+
+# Crear botón para calcular
+tk.Button(ventana, text="Calcular", bg="lightblue", font=("Arial", 16), command=tipo_triangulo).pack(pady=20)
+
+ventana.mainloop()
