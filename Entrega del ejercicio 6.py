abb1aa50b5382419a52dab96309047783c36e20f
Paco Estrada <pacoestrada77@gmail.com>
2023-04-14 11:12:05 +0200

    Version de entrega ejercicio 6

--- Entrega del ejercicio 6.py
+++ Entrega del ejercicio 6.py
@@ -5,7 +5,8 @@ with open('tiempo.json') as f:
     data = json.load(f)
 
 # imprimir la cabecera de la tabla
-print(f'{"Fecha":<15} | {"Máx Temp":<15} | {"Mín Temp":<15} | {"Viento":<15}  1 {"Humedad":<15} | {"Descripción":<15}')
+print(f'{"FECHA":<15} | {"TEMP MIN":<15} | {"TEMP MAX":<15} | {"HUMEDAD":<15} | {"VIENTO":<15} |{"DESCRIPCIÓN":<15}')
+print("----------------------------------------------------------------------------------------------------------------")
 
 
 # iterar sobre los 7 días y mostrar la información en la tabla
@@ -17,7 +18,7 @@ for i in range(1, 8):
     wind = day['wind']
     humidity = day['humidity']
     text = day['text']
-    print(f'{date:<15} {max_temp:<15} {min_temp:<15} {wind:<15} {humidity:<15} {text:<15}')
+    print(f'{date:<15} | {min_temp:<15} | {max_temp:<15} | {humidity:<15} | {wind:<15} | {text:<16}')
 
 
 
