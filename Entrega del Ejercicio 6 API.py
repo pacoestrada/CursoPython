70380d867845f554f46c4a2d7c2fc9925f300b7b
Paco Estrada <pacoestrada77@gmail.com>
2023-04-17 19:41:18 +0200

    Version 2 del ejercicio 6 con el API de Tu Tiempo

--- Entrega del Ejercicio 6 API.py
+++ Entrega del Ejercicio 6 API.py
@@ -6,16 +6,16 @@ url = "https://api.tutiempo.net/json/?lan=es&apid=zwDX4azaz4X4Xqs&ll=40.4178,-3.
 # Envíamos una solicitud HTTP GET a la URL del ejercicio 6 y guardamos la respuesta en la variable 'respuesta'
 respuesta = urllib.request.urlopen(url)
 
-"""Leemos el contenido de 'respuesta'l y lo decodificamoso decodifica a una cadena de caracteres (usando 'utf-8')
- y luego lo convierte en un objeto de Python (diccionario) usando json.loads() """
+""" Leemos el contenido de 'respuesta' y lo decodificamoso decodifica a una cadena de caracteres (usando 'utf-8')
+ y luego lo convertimos en un diccionario de Python usando json.loads() """
 data = json.loads(respuesta.read().decode('utf-8'))
 
 
-# Imprimir la cabecera de la tabla
+# Imprimimos la cabecera de la tabla
 print(f'{"FECHA":<15} | {"TEMP MIN":<15} | {"TEMP MAX":<15} | {"HUMEDAD":<15} | {"VIENTO":<15} |{"DESCRIPCIÓN":<15}')
 print("----------------------------------------------------------------------------------------------------------------")
 
-# Iterar sobre los 7 días y mostrar la información en la tabla
+# Iteramos sobre los 7 días y mostramos la información en la tabla
 for i in range(1, 8):
     day = data[f'day{i}'] # Accedemos al diccionario asociado con el día específico utilizando el valor de 'i', y asignnamos a la variable 'day'
     date = day['date']
