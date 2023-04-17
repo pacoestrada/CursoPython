504b75e65700c6a39cbf701d062483bcf3245e91
Paco Estrada <pacoestrada77@gmail.com>
2023-04-17 20:31:26 +0200

    Version 6 del ejercicio 6 con el API de Tu Tiempo

--- Entrega del Ejercicio 6 API.py
+++ Entrega del Ejercicio 6 API.py
@@ -1,18 +1,20 @@
+#Importamos los módulos que usaremos para el ejercicio.
 import json
 import urllib.request
 
-# Obtener el contenido JSON de la URL proporcionada en el ejercicio 6
+# Obtenemos el contenido JSON de la URL proporcionada en el ejercicio 6.
 url = "https://api.tutiempo.net/json/?lan=es&apid=zwDX4azaz4X4Xqs&ll=40.4178,-3.7022"
+
 # Envíamos una solicitud HTTP GET a la URL del ejercicio 6 y guardamos la respuesta en la variable 'respuesta'
 respuesta = urllib.request.urlopen(url)
 
-""" Leemos el contenido de 'respuesta' y lo decodificamoso decodifica a una cadena de caracteres (usando 'utf-8')
+""" Leemos el contenido de 'respuesta' y lo decodificamos (usando 'utf-8')
  y luego lo convertimos en un diccionario de Python usando json.loads() """
 data = json.loads(respuesta.read().decode('utf-8'))
 
 
-# Imprimimos la cabecera de la tabla
-print(f'{"FECHA":<15} | {"TEMP MIN":>15} | {"TEMP MAX":>15} | {"HUMEDAD":>15} | {"VIENTO":>15} | {"DESCRIPCIÓN":>15}')
+# Imprimimos la cabecera de la tabla, formateando el texto con alineación a la izquierda de la fecha y resto a la derecha. Damos más espacio a Descripción.
+print(f'{"FECHA":<15} | {"TEMP MIN":>15} | {"TEMP MAX":>15} | {"HUMEDAD":>15} | {"VIENTO":>15} | {"DESCRIPCIÓN":>30}')
 
 # Iteramos sobre los 7 días y mostramos la información en la tabla
 for i in range(1, 8):
